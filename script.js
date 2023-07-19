9514928883c4b18c6a55cffb2346a28083f29ce4
user-rahul <rahulgupta.dec2002@gmail.com>
2023-07-19 12:56:03 +0530

    Removed my api key

--- script.js
+++ script.js
@@ -24,22 +24,22 @@ const main = async () => {
 };
 
 const getAuthorImage = async (author) => {
-    const apiKey = 'kxzGxvzyXjO6Mx8LR5kayMohc52VSRjmo5MpwEng5rducCKlf3OTCJ9F';
-    const query = encodeURIComponent(author); // Encode the author name for URL
-  
-    const response = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=1`, {
-      headers: {
-        Authorization: apiKey
-      }
-    });
-  
-    const data = await response.json();
-    if (data.photos && data.photos.length > 0) {
-      return data.photos[0].src.medium; // Return the URL of the first image found
-    } else {
-      return null; // Return null if no images found
+  const apiKey = 'API_KEY';
+  const query = encodeURIComponent(author); // Encode the author name for URL
+
+  const response = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=1`, {
+    headers: {
+      Authorization: apiKey
     }
-  };
-  
+  });
+
+  const data = await response.json();
+  if (data.photos && data.photos.length > 0) {
+    return data.photos[0].src.medium; // Return the URL of the first image found
+  } else {
+    return null; // Return null if no images found
+  }
+};
+
 
 main();
